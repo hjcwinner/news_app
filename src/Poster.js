8dad37a08a8244e1361b8ba4f49d50bcb99c6a33
hjcdb <hjcwinner@naver.com>
2020-08-24 15:28:17 +0900

    프로퍼티 적용하기

--- src/Poster.js
+++ src/Poster.js
@@ -1,9 +1,25 @@
+// import React from 'react';
+
+// const Poster = ({poster, alt}) => {
+//     return (
+//         <img src={poster} alt={alt} />
+//     );
+// };
+
+// export default Poster;
+
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Poster = ({poster, alt}) => {
     return (
-        <img src={poster} alt={alt} />
+       <img src={poster} alt={alt} />
     );
+};
+
+Poster.propTypes = {
+    poster : propTypes.string.isRequired,
+    alt : propTypes.string
 };
 
 export default Poster;
