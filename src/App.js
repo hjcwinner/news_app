ace48a71ee3cfd93d068137b52e546f91689c8ac
hjcdb <hjcwinner@naver.com>
2020-08-24 16:00:38 +0900

    rendering process

--- src/App.js
+++ src/App.js
@@ -9,25 +9,7 @@ export default class App extends Component {
   // Render: componentWillMount() -> render() -> componentDidMount()
   // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
   state = {
-    greeting: 'Hello',
-    movies: [
-        {
-          title: "Matrix",
-          poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY1200_CR84,0,630,1200_AL_.jpg"
-        },
-        {
-          title: "Full Metal Jacket",
-          poster: "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
-        },
-        {
-          title: "Oldboy",
-          poster: "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg"
-        },
-        {
-          title: "Star Wars",
-          poster: "https://i.pinimg.com/736x/a1/61/c7/a161c7034917936f8bd53ca65bc6f0d0--star-wars-stuff-art-illustrations.jpg"
-        }
-      ]
+    
   }
 
 
@@ -35,17 +17,35 @@ export default class App extends Component {
   componentDidMount(){
     setTimeout(() => {
       this.setState({
-        movies : [
-          {
-            title: "Trainspotting",
-            poster: "https://resizing.flixster.com/OUEArjor-MbyCV6GqLU85Hk9jQI=/300x300/v1.bjs1NTQ3OTM7ajsxNzQ2NjsxMjAwOzIwMDA7MTMzMQ"
-          },
-          ...this.state.movies
-        ]
+        greeting: 'Hello',
+        movies: [
+            {
+              title: "Matrix",
+              poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY1200_CR84,0,630,1200_AL_.jpg"
+            },
+            {
+              title: "Full Metal Jacket",
+              poster: "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
+            },
+            {
+              title: "Oldboy",
+              poster: "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg"
+            },
+            {
+              title: "Star Wars",
+              poster: "https://i.pinimg.com/736x/a1/61/c7/a161c7034917936f8bd53ca65bc6f0d0--star-wars-stuff-art-illustrations.jpg"
+            }
+          ]  
       })
     }, 5000)
   }
 
+  renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies
+  }
 
 
 
@@ -54,14 +54,7 @@ export default class App extends Component {
     const { movies } = this.state
     return (
       <div className="App">
-        {movies.map(item => {
-          return (
-            <Movie title={item.title} poster={item.poster} /> 
-            // <div>
-            //   <h1>{item.title}</h1>
-            //   <img src={item.poster} alt={item.title} />
-            // </div>
-          )})}
+       {movies ? this.renderMovies() : 'Loading'}
       </div>
     )
   }
