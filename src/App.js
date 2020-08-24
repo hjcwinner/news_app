780ee5ac060af79cbd6370bc310986a6369f5abe
hjcdb <hjcwinner@naver.com>
2020-08-24 16:17:44 +0900

    networking

--- src/App.js
+++ src/App.js
@@ -15,36 +15,29 @@ export default class App extends Component {
 
   // 라이프사이클 함수
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        greeting: 'Hello',
-        movies: [
-            {
-              title: "Matrix",
-              poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY1200_CR84,0,630,1200_AL_.jpg"
-            },
-            {
-              title: "Full Metal Jacket",
-              poster: "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
-            },
-            {
-              title: "Oldboy",
-              poster: "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg"
-            },
-            {
-              title: "Star Wars",
-              poster: "https://i.pinimg.com/736x/a1/61/c7/a161c7034917936f8bd53ca65bc6f0d0--star-wars-stuff-art-illustrations.jpg"
-            }
-          ]  
-      })
-    }, 5000)
+    this.getmovies()
   }
+
+
+
+
 
   renderMovies = () => {
     const movies = this.state.movies.map((movie, index) => {
       return <Movie title={movie.title} poster={movie.poster} key={index} />
     })
     return movies
+  }
+
+  getmovies = async() => {
+    this.callapi()
+  }
+
+  callapi = () => {
+    return fetch("http://newsapi.org/v2/everything?q=bitcoin&from=2020-07-24&sortBy=publishedAt&apiKey=51bb0125448e42c7b90700544eca7361")
+    .then(data => data.json())
+    .then(json => console.log(json.articles))
+    .catch(err => console.log(err))
   }
 
 
