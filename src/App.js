2c8faf44c32e29243d3e821905b481ee59bb8e4a
hjcdb <hjcwinner@naver.com>
2020-08-24 14:47:03 +0900

    add image #2

--- src/App.js
+++ src/App.js
@@ -26,8 +26,14 @@ export default class App extends Component {
     return (
       <div className="App">
         {movies.map(item => {
-          return <h1>{item.title}</h1>
-        })}
+          return (
+            <div>
+              <h1>{item.title}</h1>
+              <img src={item.poster} alt={item.title} />
+            </div>
+            
+          )})}
+
         
       </div>
     )
